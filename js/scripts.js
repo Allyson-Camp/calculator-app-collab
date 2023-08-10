c48ddfc77a956417fcba7690f30e981e11d49823
Allyson Camp <Allysoncamp92@gmail.com>
2023-08-10 11:04:00 -0700

    add calculation functions
    Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+// needed:
+  // onload function
+  //query select or getbyid all elements needed
+  //event listener for submit button so we know its been clicked
+  //if/else statement for calculation to be performed
+  //variable for result
+
+  //Business logic
+  // calculation functions
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function sub(num1, num2) {
+  return num1 - num2;
+}
+
+function mult(num1, num2) {
+  return num1 * num2;
+}
+
+function div(num1, num2) {
+  return num1 / num2;
+}
+
+  //UI logic
+  //events and other things that make the functions run
+
