31110c785c429eeabb63a832bf61e89917d18e36
Allyson Camp <Allysoncamp92@gmail.com>
2023-08-10 11:58:56 -0700

    WIP submit eventlistener for form
     Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // needed:
 // onload function DONE
-//query select or getbyid all elements needed
+//query select or getbyid all elements needed DONE
 //event listener for submit button so we know its been clicked
-//if/else statement for calculation to be performed
+//if/else statement for calculation to be performed DONE
 //variable for result
 
 //Business logic
@@ -26,6 +26,24 @@ function div(num1, num2) {
 //events and other things that make the functions run
 
 window.addEventListener("load", function () {
-    let form = document.querySelector("form#input-form");
-    let submitBtn = document.querySelector("button#submit");
+  const form = document.querySelector("form#input-form");
+  let submitBtn = document.querySelector("button#submit");
+  const num1 = parseInt(document.querySelector("form#number-input1").value);
+  const num2 = parseInt(this.document.querySelector("form#number-input2").value);
+  const operation = document.querySelector("form#operation");
+
+form.addEventListener("submit"), function(event) {
+    event.preventDefeault();
+  
+  let result = "";
+    if (operation === "add") {
+        result = add(num1, num2);
+    } else if (operation === "sub") {
+        result = sub(num1, num2);
+    } else if (operation === "mult") {
+        result = mult(num1, num2);
+    } else if (operation === "div") {
+        result = div(num1, num2);
+    }
+}
 });
